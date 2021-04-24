7bdc7f8eebddf4ae84f541c33e2765a231e63893
am2466ya@go.minnstate.edu <60166295+shagur@users.noreply.github.com>
2021-04-24 16:09:22 -0500

    update

--- src/main/java/com/cs499/repositories/MembersActivityRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cs499.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cs499.model.MembersActivity;
-
-public interface MembersActivityRepo extends JpaRepository<MembersActivity, Long> {
-
-}
